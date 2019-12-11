74b1e80ebcb9b841414667818ec28aa94207a0a6
Houston Knight <houstonknight3434@gmail.com>
2019-12-11 15:46:07 -0600

    Make app production ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require.context('../images', true)
 
 import '../stylesheets/application'
 import './bootstrap_custom.js'
