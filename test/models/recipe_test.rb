2565f2ce416fb0bfe6a5ddb956cc32786d3ee4f5
Houston Knight <houstonknight3434@gmail.com>
2020-01-02 16:53:18 -0600

    Update Ruby version and dependencies

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+
+  def setup
+    @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe")
+  end
+
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+end
